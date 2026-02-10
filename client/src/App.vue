ec100c4f88cb7700dbf421d01325e9a3f77d9609
Saul Graves <graves.saul@gmail.com>
2026-02-09 21:51:56 -0500

    create web app entry

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app-shell">
+    <AppHeader />
+    <main class="main-content">
+      <RouterView />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import AppHeader from '@/components/layout/AppHeader.vue'
+import AppFooter from '@/components/layout/AppFooter.vue'
+import { useFavoritesStore } from '@/stores/favorites'
+
+const favoritesStore = useFavoritesStore()
+
+onMounted(() => {
+  favoritesStore.fetchFavorites()
+})
+</script>
